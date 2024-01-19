1cb331915c4828e757d4ae7194534669374fe0b9
Wagner Cardoso <wagnerdev01@gmail.com>
2024-01-19 18:49:44 -0300

    Add cors to api

--- api/src/infra/app.ts
+++ api/src/infra/app.ts
@@ -1,12 +1,14 @@
 import 'reflect-metadata'
 import '../containers'
 import express from 'express'
+import cors from 'cors'
 import { routes } from './routes'
 import { catchAllErrors } from './http/middlewares/catch-all-errors'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 app.use(catchAllErrors)
 
