f68919767aebf708ecb87bb7b7693d546630e420
Wagner Cardoso <wagnerdev01@gmail.com>
2024-01-19 22:56:21 -0300

    Improve readabilty on registered customers component

--- web/src/features/customers/components/cards/registered-customers.tsx
+++ web/src/features/customers/components/cards/registered-customers.tsx
@@ -8,14 +8,14 @@ import { useCustomersCount } from '../../hooks/use-customers-count'
 import { Spinner } from '@/common/components/spinner'
 
 export function RegisteredCustomersCard() {
-  const { count: customersCount, isFetching: isLoading } = useCustomersCount()
+  const { count: customersCount, isFetching } = useCustomersCount()
   return (
     <Card className="w-[512px] py-6 border-blue-500">
       <CardHeader>
         <CardTitle className="text-blue-700">Clientes cadastrados</CardTitle>
       </CardHeader>
       <CardContent className="text-blue-500">
-        {isLoading ? (
+        {isFetching ? (
           <Spinner />
         ) : (
           <span className="font-medium text-8xl">{customersCount}</span>
