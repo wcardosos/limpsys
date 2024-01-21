f84a48ccad08541033e6f4606bdbb904aac4d097
Wagner Cardoso <wagnerdev01@gmail.com>
2024-01-21 15:24:14 -0300

    (web) Add error boundary

---
+++ web/src/_error.tsx
@@ -0,0 +1,7 @@
+export function ErrorBoundaryFallback() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <p>Não foi possível acessar essa página. Tente novamente.</p>
+    </div>
+  )
+}
--- web/src/app.tsx
+++ web/src/app.tsx
@@ -3,17 +3,21 @@ import { Router } from './router'
 import { CreateCustomerDialogProvider } from './features/customers/contexts/create-customer-dialog'
 import { CustomersProvider } from './features/customers/contexts/customers'
 import { RouteCalculationProvider } from './features/route/contexts/route-calculation'
+import { ErrorBoundary } from 'react-error-boundary'
+import { ErrorBoundaryFallback } from './_error'
 
 export function App() {
   return (
-    <BrowserRouter>
-      <CustomersProvider>
-        <CreateCustomerDialogProvider>
-          <RouteCalculationProvider>
-            <Router />
-          </RouteCalculationProvider>
-        </CreateCustomerDialogProvider>
-      </CustomersProvider>
-    </BrowserRouter>
+    <ErrorBoundary fallback={<ErrorBoundaryFallback />}>
+      <BrowserRouter>
+        <CustomersProvider>
+          <CreateCustomerDialogProvider>
+            <RouteCalculationProvider>
+              <Router />
+            </RouteCalculationProvider>
+          </CreateCustomerDialogProvider>
+        </CustomersProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
   )
 }
